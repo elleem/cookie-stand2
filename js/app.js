426729f39be796fc0a264e404a4eeb72c3091db9
Lauren Main <lauren.main28@gmail.com>
2022-05-21 19:25:15 -0700

    add constructor, prototype, and instantiate sales

--- js/app.js
+++ js/app.js
@@ -1,15 +1,10 @@
 "use-strict";
-//cookie shop
 //it use the variables min/max customer
 //average cookies
 //location
 //to provide total sales per hour
 //total sales per day
 //use math.random
-
-//  function averageSpend(min, max){
-//    let
-//  }
 
 //global variables at the top
 
@@ -32,50 +27,64 @@ let operationHours = [
   "6pm",
   "7pm",
 ];
-
-let seattle = {
-  name: "Seattle",
-  maxCustomers: 65,
-  minCustomers: 23,
-  averageCookiesSold: 6.3,
-  soldPerHour: [],
-  dailySold: 0,
-};
-
 //min&max are inclusive mdn I should refer to the class repo instead of the mdn
 function custNumber(max, min) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+//constructor
+function StoreLocation(name, maxCustomers, minCustomers, averageCookiesSold) {
+  this.name = name;
+  this.maxCustomers = maxCustomers;
+  this.minCustomers = minCustomers;
+  this.averageCookiesSold = averageCookiesSold;
+  this.soldPerHour = [];
+  this.dailySold = 0;
+}
 
-//custNumber(seattle.maxCustomers,seattle.minCustomers);
-//console.log (salmonCustomers);
-//paris, go back to see that we passed instructor into the function
+//instantiate sales per city
+new StoreLocation("Seattle", 65, 23, 6.3);
+new StoreLocation("Tokyo", 24, 3, 1.2);
+new StoreLocation("Dubai", 38, 11, 3.7);
+new StoreLocation("Paris", 38, 20, 2.3);
+new StoreLocation("Lima", 16, 2, 4.6);
+//console.log(StoreLocation5);
 
-function cookiesSoldPerHour() {
+//method for finding total cookies sold per hour and tallies daily total sold
+
+StoreLocation.prototype.cookiesSoldPerHour = function () {
   for (let i = 0; i < operationHours.length; i++) {
-    let salmonCustomers = custNumber(seattle.maxCustomers, seattle.minCustomers);
+    let salmonCustomers = custNumber(this.maxCustomers, this.minCustomers);
     let salmonCookiesSold = Math.ceil(
-      salmonCustomers * seattle.averageCookiesSold
+      salmonCustomers * this.averageCookiesSold
     );
-    seattle.soldPerHour.push(salmonCookiesSold);
-    seattle.dailySold += salmonCookiesSold;
-    //console.log (salmonCookiesSold);
+    this.soldPerHour.push(salmonCookiesSold);
+    this.dailySold += salmonCookiesSold;
+    console.log(salmonCookiesSold);
   }
-}
-cookiesSoldPerHour();
-
-function render() {
+};
+//render each cookie stand
+StoreLocation.prototype.render = function () {
   //seattle.cookiesSoldPerHour();
-  let cookiesInSeattle = document.getElementById("seattle-cookiestand");
-  for (let i = 0; i < operationHours.length; i++) {
-    let li = document.createElement("li");
-    li.textContent = `${operationHours[i]}: ${seattle.soldPerHour[i]}`;
-    cookiesInSeattle.appendChild(li);
+  let containerElem = document.getElementById("stores");
+  let rowElem = document.createElement("tr");
+  for (let i = 0; i < this.operationHours.length; i++) {
+    dataElem.innerText = this.rowInformation[i];
+    rowElem.appendChild(dataElem);
   }
-}
+  containerElem.apprendChild(rowElem);
+};
 
-render();
-let li = document.createElement("li");
-li.textContent = `Daily Total: ${seattle.dailySold}`;
-cookiesInSeattle.appendChild(li);
+// let li = document.createElement("li");
+// li.textContent = `Daily Total: ${this.dailySold}`;
+// cookiesInSeattle.appendChild(li);
 
+// function StoreLocation (name, maxCustomers, minCustomers, averageCookiesSold){
+//   this.name = name;
+//   this.maxCustomers= maxCustomers;
+//   this.minCustomers = minCustomers;
+//   this.averageCookiesSold =averageCookiesSold;
+// }
+
+//console.log(cookiesSoldPerHour);
+cookiesSoldPerHour();
+newStoreLocation.render();
