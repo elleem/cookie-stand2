5d52c5a65fed9ba5a75921cf6d6d86a15f0b9ca2
Lauren Main <lauren.main28@gmail.com>
2022-06-01 20:19:38 -0700

    first pass at forms, exact copy of petshop notes

--- js/app.js
+++ js/app.js
@@ -43,8 +43,6 @@ function StoreLocation(name, maxCustomers, minCustomers, averageCookiesSold) {
   this.dailySold = 0;
 }
 
-
-
 //create object sales per city
 let store1 = new StoreLocation("Seattle", 65, 23, 6.3);
 let store2 = new StoreLocation("Tokyo", 24, 3, 1.2);
@@ -53,13 +51,15 @@ let store4 = new StoreLocation("Paris", 38, 20, 2.3);
 let store5 = new StoreLocation("Lima", 16, 2, 4.6);
 //console.log(StoreLocation5);
 
-let allStores = [store1, store2, store3, store4, store5,]; 
+let allStores = [store1, store2, store3, store4, store5];
 
 //method for finding total cookies sold per hour and tallies daily total sold
 
 StoreLocation.prototype.cookiesSoldPerHour = function () {
   for (let i = 0; i < operationHours.length; i++) {
-    let salmonCookiesSold = Math.ceil(custNumber(this.maxCustomers, this.minCustomers) * this.averageCookiesSold);
+    let salmonCookiesSold = Math.ceil(
+      custNumber(this.maxCustomers, this.minCustomers) * this.averageCookiesSold
+    );
     this.dailySold += salmonCookiesSold;
     this.soldPerHour.push(salmonCookiesSold);
     //console.log(salmonCookiesSold);
@@ -78,58 +78,54 @@ StoreLocation.prototype.render = function () {
     dataCell.textContent = this.soldPerHour[i];
     trOneElem.appendChild(dataCell);
   }
-  let dataCellTotal = document.createElement('td');
+  let dataCellTotal = document.createElement("td");
   dataCellTotal.textContent = this.dailySold;
   trOneElem.appendChild(dataCellTotal);
   return this.soldPerHour;
-
 };
 
-
-  function setTableHeader() {
-    let newRow = document.createElement("tr");
-    myContainer.appendChild(newRow);
+function setTableHeader() {
+  let newRow = document.createElement("tr");
+  myContainer.appendChild(newRow);
+  let thElem = document.createElement("th");
+  newRow.appendChild(thElem);
+  thElem.textContent = "Store Location";
+  for (let i = 0; i < operationHours.length; i++) {
     let thElem = document.createElement("th");
     newRow.appendChild(thElem);
-    thElem.textContent = "Store Location";
-    for (let i = 0; i < operationHours.length; i++) {
-      let thElem = document.createElement("th");
-      newRow.appendChild(thElem);
-      thElem.textContent = `${operationHours[i]}`;
-    }
+    thElem.textContent = `${operationHours[i]}`;
+  }
   let thTotal = document.createElement("th");
   newRow.appendChild(thTotal);
   thTotal.textContent = "Daily Location Total";
-}; 
-
+}
 
 function setTableFooter() {
-  let footElem = document.createElement('tfoot');
+  let footElem = document.createElement("tfoot");
   myContainer.appendChild(footElem);
-  let newRow = document.createElement('tr');
+  let newRow = document.createElement("tr");
   footElem.appendChild(newRow);
 
-  let tdElem = document.createElement('td');
-  tdElem.textContent = 'Totals';
+  let tdElem = document.createElement("td");
+  tdElem.textContent = "Totals";
   newRow.appendChild(tdElem);
 
-  let grandTotal=0;
-  for (let i = 0; i < operationHours.length; i++)  {
-    let hourlyTotal = 0; 
-    for (let j =0; j < allStores.length; j++) {   // iterate over the locations
-      hourlyTotal +=(allStores[j].soldPerHour[i]);
-      grandTotal += (allStores[j].soldPerHour[i]);
+  let grandTotal = 0;
+  for (let i = 0; i < operationHours.length; i++) {
+    let hourlyTotal = 0;
+    for (let j = 0; j < allStores.length; j++) {
+      // iterate over the locations
+      hourlyTotal += allStores[j].soldPerHour[i];
+      grandTotal += allStores[j].soldPerHour[i];
     }
-    let dataCell = document.createElement('td'); 
+    let dataCell = document.createElement("td");
     dataCell.textContent = `${hourlyTotal}`;
     newRow.appendChild(dataCell);
   }
-  let totalCell = document.createElement('td');
+  let totalCell = document.createElement("td");
   totalCell.textContent = grandTotal;
   newRow.appendChild(totalCell);
-};
-
-
+}
 
 setTableHeader();
 
@@ -139,4 +135,37 @@ store3.render();
 store4.render();
 store5.render();
 
-setTableFooter(); //put it a reading order becuase they are building on each other
+//setTableFooter(); //put it a reading order becuase they are building on each other
+ 
+ 
+ 
+
+function onSubmit(event) {
+  event.preventDefault();
+  let form = event.target;
+
+  let name = form["name"].value;
+  let maxCustomers = Number(form["maxCustomers"].value);
+  let minCustomers = Number(form["minCustomers"].value);
+  let averageCookiesSold = Number(form["averageCookiesSold"].value);
+
+  let location = {
+    name: name,
+    maxCustomers: maxCustomers,
+    minCustomers: minCustomers,
+    averageCookiesSold: averageCookiesSold,
+  };
+  allLocations.push(submit);
+  for (let i = 0; i <allLocations.length; i++){
+    let location = allLocations[i];
+    location.render();
+   }
+   makeTotalsRow ();
+}
+//name, maxCustomers, minCustomers, averageCookiesSold
+
+
+
+
+
+document.getElementById("expansion").addEventLister("submit", onSubmit);
